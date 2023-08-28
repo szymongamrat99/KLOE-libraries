3d199368b1e5c7c66a5be9beb492c255a421161b
Szymon Gamrat <szymon.gamrat@student.uj.edu.pl>
2023-08-28 16:37:33 +0200

    16:37

--- Codes/interference.h
+++ Codes/interference.h
@@ -13,94 +13,92 @@
 #include <TFile.h>
 #include <vector>
 
-#define chann_num 9
-
 namespace KLOE
 {
   class interference : public pm00
   {
-  public:
-    UInt_t bin_number = 181;
-    Double_t x_min = -90., x_max = 90.;
+    public:
 
-    std::vector <Double_t> time_diff[chann_num], time_diff_gen;
+      std::vector<Double_t> time_diff[chann_num], time_diff_gen;
+      std::vector<Double_t> time_diff_rand_mc[chann_num], time_diff_rand_data[chann_num], time_diff_gen_rand_mc, time_diff_gen_rand_data;
 
-    UInt_t *ev_num;
+      //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////
+      interference(TString mode_init, UInt_t *evnum_init, UInt_t bin_num_init, Double_t x_min_init, Double_t x_max_init) : mode(mode_init)
+      {
 
-    interference(TString mode_init, UInt_t *ev_num_init, UInt_t bin_num, Double_t x_min_init, Double_t x_max_init) : mode(mode_init), bin_number(bin_num), x_min(x_min_init), x_max(x_max_init)
-    {
-      ev_num = new UInt_t[chann_num];
-      for (Int_t i = 0; i < chann_num; i++)
-        ev_num[i] = ev_num_init[i];
+        bin_number = bin_num_init;
+        x_min = x_min_init;
+        x_max = x_max_init;
 
-      time_diff_gen = new Double_t[ev_num[0]];
-      time_diff = new Double_t *[chann_num];
+        evnum = new UInt_t[chann_num];
 
-      for (Int_t i = 0; i < chann_num; i++)
-        time_diff[i] = new Double_t[ev_num[i]];
+        for(Int_t i = 0; i < chann_num; i++) evnum[i] = evnum_init[i];
 
-      for (Int_t i = 0; i < chann_num; i++)
-        frac[i] = new TH1F(("Fitted histo " + std::to_string(i)).c_str(), "", bin_number, x_min, x_max);
+        //////////////////////////////////////////////////////////////////////////////////
 
-      corr_vals = new Double_t[bin_number];
+        left_x_split = -30.0;
+        center_x_split = 0.0;
+        right_x_split = 30.0;
 
-      left_x_split = -30.0;
-      center_x_split = 0.0;
-      right_x_split = 30.0;
+        //////////////////////////////////////////////////////////////////////////////////
 
-      //////////////////////////////////////////////////////////////////////////////////
+        for (Int_t i = 0; i < chann_num; i++)
+          frac[i] = new TH1F(("Fitted histo " + std::to_string(i)).c_str(), "", bin_number, x_min, x_max);
 
-      TFile file("/internal/big_one/4/users/gamrat/scripts/Scripts/corr_file.root");
+        //////////////////////////////////////////////////////////////////////////////////////////////////
 
-      corr_factor = (TGraphErrors *)file.Get("correction_factor");
-      corr_vals = corr_factor->GetY();
+        corr_vals = new Double_t[bin_number];
 
-      file.Close();
+        TFile file("/internal/big_one/4/users/gamrat/scripts/Scripts/corr_file.root");
 
-      ///////////////////////////////////////////////////////////////////////////////////
-    };
+        corr_factor = (TGraphErrors *)file.Get("correction_factor");
+        corr_vals = corr_factor->GetY();
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////
+        file.Close();
 
-    void bin_extraction(UInt_t channel, TH1 *histogram);
+        ///////////////////////////////////////////////////////////////////////////////////
+      };
 
-    void mc_randomize();
+      //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////
+      void bin_extraction(UInt_t channel, TH1 *histogram);
 
-    Double_t interf_function(const Float_t x, Int_t check = 1, const Double_t *par = 0);
+      void mc_randomize();
 
-    Double_t interf_chi2_split(const Double_t *xx);
-    Double_t interf_chi2_window(const Double_t *xx);
-    Double_t interf_chi2_excluded(const Double_t *xx);
-    Double_t interf_chi2_all(const Double_t *xx);
+      //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    Double_t interf_chi2_mc(const Double_t *xx);
-    Double_t interf_chi2_bcg(const Double_t *xx);
+      Double_t interf_function(const Float_t x, Int_t check = 1, const Double_t *par = 0);
 
-    Double_t interf_chi2(const Double_t *xx);
+      Double_t interf_chi2_split(const Double_t *xx);
+      Double_t interf_chi2_window(const Double_t *xx);
+      Double_t interf_chi2_excluded(const Double_t *xx);
+      Double_t interf_chi2_all(const Double_t *xx);
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////////////////////////////////////////
+      Double_t interf_chi2_mc(const Double_t *xx);
+      Double_t interf_chi2_bcg(const Double_t *xx);
 
-  private:
-    TString mode = "split"; // "split", "window", "excluded, "mc", "bcg", "all"
-    TH1 *frac[chann_num];
-    TGraphErrors *corr_factor;
+      Double_t interf_chi2(const Double_t *xx);
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////
+      /////////////////////////////////////////////////////////////////////////////////////////////////////
+      /////////////////////////////////////////////////////////////////////////////////////////////////////
+      /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    std::vector<UInt_t> indices[chann_num];
-    std::vector<Double_t> b[chann_num], e[chann_num];
-    std::vector<Double_t> time_diff_rand_mc[chann_num], time_diff_rand_data[chann_num], time_diff_gen_rand_mc, time_diff_gen_rand_data;
+    private:
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////
+      TString mode = "split"; // "split", "window", "excluded, "mc", "bcg", "all"
+      TGraphErrors *corr_factor;
 
-    Double_t *exclusions, left_x_split, center_x_split, right_x_split;
-    Double_t *corr_vals;
+      /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      std::vector<UInt_t> indices[chann_num];
+      std::vector<Double_t> b[chann_num], e[chann_num];
+
+      /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      UInt_t *evnum;
+      Double_t *exclusions, left_x_split, center_x_split, right_x_split;
+      Double_t *corr_vals;
 
   };
 } // namespace KLOE
--- Codes/kloe_class.h
+++ Codes/kloe_class.h
@@ -15,9 +15,14 @@ namespace KLOE
             TLorentzVector pi_ne_mom[2], pi_ne_pos[2];
             TLorentzVector photon_mom[4], photon_pos[4];
 
+            UInt_t bin_number;
+            Double_t x_min, x_max, y_min, y_max;
+
             Double_t inv_mass;
             TVector3 boost;
             Double_t angle_vec, transv, azim_angle;
+
+            TH1 *frac[chann_num];
 
             pm00(TLorentzVector *mom_list, TLorentzVector *pos_list);
 
@@ -29,6 +34,7 @@ namespace KLOE
 
             void boost_vector(TLorentzVector four_mom);
             void lorentz_transf(TLorentzVector four_mom);
+
     };
 }
 
