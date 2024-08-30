a77099703c124a0841df2c7b9bfc8bea903b8f62
Szymon Gamrat <szymon.gamrat@student.uj.edu.pl>
2024-08-30 23:05:45 +0200

    20240830_2305

--- prod2root_analysis_code/compile_fortran.sh
+++
@@ -1,4 +0,0 @@
-g++ -c fortran_to_cpp_linking.cpp
-gfortran -c analysis_library.f
-gfortran -c analysismodule.f
-g++ -o klspm00_analysis.exe analysismodule.o analysis_library.o fortran_to_cpp_linking.o -lgfortran -llapack -lblas -L$CERN -lkernlib
